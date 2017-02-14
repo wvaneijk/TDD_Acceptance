64931e8045d3b35051236ba2199830430249b522
William van Eijk <WilliamvanEijk@gmail.com>
2017-02-14 21:59:20 +0100

    Hello world

--- src/main/java/main.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by William on 14/02/17.
- */
-public class main {
-}
---
+++ src/main/java/mainClass.java
@@ -0,0 +1,11 @@
+/**
+ * Created by William on 14/02/17.
+ */
+public class mainClass {
+
+    public static void main(String[] args) {
+
+        System.out.println(" henk" );
+        for (int i = 0; i < 25; i++) System.out.println(i);
+    }
+}
